c689f5854cc14ca9cb158b85065bf145dbb2a098
xinyuow <365mxy@gmail.com>
2022-03-08 14:55:28 +0800

    修正漏洞，更新基础代码生成工具

--- src/main/java/com/xinyuow/frame/common/mybatis/CodeGenerator.java
+++ src/main/java/com/xinyuow/frame/common/mybatis/CodeGenerator.java
@@ -1,5 +1,6 @@
 package com.xinyuow.frame.common.mybatis;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
@@ -10,6 +11,7 @@ import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,17 +61,32 @@ public class CodeGenerator {
     // 自定义 model.java 的类路径
     private final static String ENTITY_PATH = "model.app";
 
+    // 自动填充的表字段名称-创建时间
+    private final static String TABLE_FIELD_FILL_CREATE_DATE = "create_date";
+    // 自动填充的表字段名称-修改时间
+    private final static String TABLE_FIELD_FILL_MODIFY_DATE = "modify_date";
+
 
     /**
      * 生成基础代码
+     * 需要修改的内容：
+     * 1.代码生成位置：{@link CodeGenerator#PROJECT_PATH}
+     * 2.开发人员名称：{@link CodeGenerator#AUTHOR}
+     * 3.MySQL的链接信息：{@link CodeGenerator#DB_URL}
+     * 4.通用表前缀：{@link CodeGenerator#TABLE_PREFIX}
+     * 5.自动填充的表字段名称-创建时间：{@link CodeGenerator#TABLE_FIELD_FILL_CREATE_DATE}
+     * 6.自动填充的表字段名称-修改时间：{@link CodeGenerator#TABLE_FIELD_FILL_MODIFY_DATE}
+     * <p>
      * 注意：新生成的代码会覆盖原有代码！！！
      */
     public static void main(String[] args) {
         // 待生成的表名
         String tableName = "";
 
-        // 调用基础代码生成方法
-        generateCode(tableName);
+        if (StringUtils.isNotBlank(tableName)) {
+            // 调用基础代码生成方法
+            generateCode(tableName);
+        }
     }
 
     /**
@@ -85,14 +102,14 @@ public class CodeGenerator {
         GlobalConfig gc = new GlobalConfig();
         gc.setOutputDir(OUTPUT_DIR);
         gc.setAuthor(AUTHOR);
-        gc.setOpen(false);
-        gc.setBaseColumnList(true);
-        gc.setBaseResultMap(true);
-        gc.setFileOverride(true);   // 覆盖已有文件
+        gc.setOpen(Boolean.FALSE);
+        gc.setBaseColumnList(Boolean.TRUE);
+        gc.setBaseResultMap(Boolean.TRUE);
+        gc.setFileOverride(Boolean.TRUE);   // 覆盖已有文件
         gc.setDateType(DateType.TIME_PACK); // 时间类型，使用 java.time 包下的新时间类型
-        gc.setSwagger2(true);
-        gc.setEnableCache(false);   // XML二级缓存
-        gc.setIdType(IdType.ASSIGN_ID); // 雪花算法生成ID
+        gc.setSwagger2(Boolean.TRUE);
+        gc.setEnableCache(Boolean.FALSE);   // XML二级缓存
+        gc.setIdType(IdType.ASSIGN_ID);     // 雪花算法生成ID
         // 自定义文件命名，%s会自动填充表实体属性
         gc.setControllerName("%sController");
         gc.setServiceName("%sService");
@@ -121,23 +138,23 @@ public class CodeGenerator {
 
         // 策略配置
         StrategyConfig sc = new StrategyConfig();
-        sc.setCapitalMode(true);    // 驼峰命名
-        sc.setEntityLombokModel(true);  // 实体类是否带有lombok风格
-        sc.setEntityTableFieldAnnotationEnable(true);   // 允许字段自动注解
-        sc.setEntitySerialVersionUID(true); // 实体生成serialVersionUID
-        sc.setRestControllerStyle(true);    // rest风格controller
-        sc.setChainModel(true); // 实体类是否为链式模型 @Accessors(chain = true)
-        sc.setControllerMappingHyphenStyle(true); // 驼峰转连字符
-        sc.setTablePrefix(TABLE_PREFIX);    // 表前缀
-        sc.setNaming(NamingStrategy.underline_to_camel);    // 表名生成策略(下划线转驼峰命名)
+        sc.setCapitalMode(Boolean.TRUE);                        // 驼峰命名
+        sc.setEntityLombokModel(Boolean.TRUE);                  // 实体类是否带有lombok风格
+        sc.setEntityTableFieldAnnotationEnable(Boolean.TRUE);   // 允许字段自动注解
+        sc.setEntitySerialVersionUID(Boolean.TRUE);             // 实体生成serialVersionUID
+        sc.setRestControllerStyle(Boolean.TRUE);                // rest风格controller
+        sc.setChainModel(Boolean.TRUE);                         // 实体类是否为链式模型 @Accessors(chain = true)
+        sc.setControllerMappingHyphenStyle(Boolean.TRUE);       // 驼峰转连字符
+        sc.setTablePrefix(TABLE_PREFIX);                        // 表前缀
+        sc.setNaming(NamingStrategy.underline_to_camel);        // 表名生成策略(下划线转驼峰命名)
         sc.setColumnNaming(NamingStrategy.underline_to_camel);  // 列名生成策略(下划线转驼峰命名)
-        sc.setSuperControllerClass(SUPER_CONTROLLER_CLASS); // 自定义controller父类
-        sc.setInclude(tableName.split(","));   // 修改替换成需要包含的表名
+        sc.setSuperControllerClass(SUPER_CONTROLLER_CLASS);     // 自定义controller父类
+        sc.setInclude(tableName.split(StrUtil.COMMA));          // 修改替换成需要包含的表名
 
         // 自定义需要填充的字段 数据库中的字段
         List<TableFill> tableFillList = new ArrayList<>();
-        tableFillList.add(new TableFill("create_date", FieldFill.INSERT));
-        tableFillList.add(new TableFill("modify_date", FieldFill.INSERT_UPDATE));
+        tableFillList.add(new TableFill(TABLE_FIELD_FILL_CREATE_DATE, FieldFill.INSERT));
+        tableFillList.add(new TableFill(TABLE_FIELD_FILL_MODIFY_DATE, FieldFill.INSERT_UPDATE));
         // 自动填充设置
         sc.setTableFillList(tableFillList);
         mpg.setStrategy(sc);
